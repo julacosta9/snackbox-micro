f5e2876783b5b2954ec9f40794b6e4fe7824099a
Julian Acosta <julacosta9@gmail.com>
2023-01-15 23:44:15 -0800

    finish first version of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,74 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="my-20 flex shrink-0 grow-0 bg-base-200 py-4">
-      <div className="flex-1 justify-center text-center">
-        <div>Add footer</div>
+    <footer className="footer mb-6 rounded-lg bg-base-200 p-10 text-base-content">
+      <div>
+        <div className="flex-0 inline-flex cursor-pointer text-lg font-extrabold leading-normal text-gray-700 transition-all duration-200 lg:text-3xl">
+          <span className="lowercase text-secondary transition">sbmicro</span>{" "}
+          <span className="capitalize text-base-content">hub</span>
+        </div>
+        <p className="max-w-sm leading-6">
+          This site is not affiliated with the real makers of the Snackbox
+          Micro, Junkfood Custom Arcades.
+          <br />
+          <br />
+          It's simply a site created by a fan. 🔮
+          <br />
+          You can check out my{" "}
+          <a
+            href="https://github.com/julacosta9"
+            target="_blank"
+            className="link link-hover text-accent"
+          >
+            GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.julianacosta.dev/"
+            target="_blank"
+            className="link link-hover text-accent"
+          >
+            portfolio
+          </a>
+          .
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Junkfood Custom Arcades</span>
+        <a
+          href="https://junkfoodarcades.com/"
+          target="_blank"
+          className="link link-hover"
+        >
+          Website (buy a micro here!)
+        </a>
+        <a
+          href="https://discord.gg/egfTusUpPY"
+          target="_blank"
+          className="link link-hover"
+        >
+          Discord
+        </a>
+        <a
+          href="http://youtube.com/channel/UCoIIhYXah87yEV_PrS4ASTw"
+          target="_blank"
+          className="link link-hover"
+        >
+          YouTube
+        </a>
+        <a
+          href="https://twitter.com/junkfoodarcades"
+          target="_blank"
+          className="link link-hover"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://www.instagram.com/junkfoodarcades/?igshid=YWJhMjlhZTc%3D"
+          target="_blank"
+          className="link link-hover"
+        >
+          Instagram
+        </a>
       </div>
     </footer>
   );
